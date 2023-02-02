b53296b639b0a4a2f1fe061659b0e5547549b64e
Laura Connor <itslauraroseco@gmail.com>
2023-02-01 23:54:26 -0500

    fixed case on App.js input

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Form from './components/Form';
+import Form from './components/form';
 import Navbar from './components/Navbar'
 import Table from './components/Table'
 
